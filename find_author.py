b3b1090d3193f453230ea95902f4745c134d8cc8
Vineet Kumar <vineeku6@in.ibm.com>
2021-05-01 12:09:19 +0530

    Find series, author

---
+++ find_author.py
@@ -0,0 +1,175 @@
+
+import argparse
+import csv
+import pandas as pd
+import requests
+import re
+import logging
+import time
+import os
+import pickle as pkl
+
+from utils import *
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+GR_URL = "http://goodreads.com"
+NEG_RESULT = "0 result(s)"
+
+
+def get_book_names_and_url(author_url, skip_books, all_books):
+    r = requests.get(author_url)
+    if r.status_code != 200:
+        logging.info(f'Series URL: bad request {r.status_code}')
+        return []
+
+    soup = BeautifulSoup(r.text, features="html.parser")
+
+    num_added = 0
+    for book in soup.find_all(class_='bookTitle'):
+        if book["href"].find("/book/show") == -1:
+            continue
+
+        book_name = book.text.strip()
+        if book_name in skip_books:
+            continue
+
+        if book_name in all_books:
+            logging.info(f'{book_name} already present')
+            continue
+
+        logging.info(book_name)
+        book_url = f'{GR_URL}{book["href"]}'
+        logging.info(book_url)
+
+        book_editions_url = find_editions_page(book_url)
+        print(book_editions_url)
+
+        isbns = get_isbns(book_editions_url)
+        logging.info(f'#ISBNs {len(isbns)}')
+
+        bookchor_present = [
+            isbn
+            for isbn in isbns
+            if is_bookchor_instock(isbn)
+        ]
+
+        logging.info(f'#BookChor: {len(bookchor_present)}')
+
+        shbi_present = is_shbi_instock(book_name)
+        logging.info(f'shbi: {shbi_present}')
+
+        all_books[book_name] = (book_name, book_url, book_editions_url, isbns, bookchor_present, shbi_present)
+        num_added += 1
+        logging.info(f'Done Book# {num_added}')
+    return num_added
+
+
+def write_csv(books, csv_name):
+    with open(csv_name, 'w') as fw:
+        writer = csv.writer(fw)
+        writer.writerow(['Book Name', 'URL', 'Editions', 'ISBNS', 'Bookchor', 'SHBI'])
+        for book_name in books:
+            book = books[book_name]
+            datum = []
+            datum.append(book[0])
+            datum.append(book[1])
+            datum.append(book[2])
+            isbns = [
+                str(num)
+                for num in book[3]
+            ]
+
+            bc_isbns = [
+                str(num)
+                for num in book[4]
+            ]
+
+            datum.append(','.join(isbns))
+            datum.append(','.join(bc_isbns))
+            datum.append(book[5])
+            writer.writerow(datum)
+
+
+def write_excel(csv_name, report_name):
+    writer = pd.ExcelWriter(report_name, engine='xlsxwriter')
+    workbook = writer.book
+    text_wrap_format = workbook.add_format({'text_wrap': True})
+
+    number_format = workbook.add_format({'num_format': '#'})
+
+    df = pd.read_csv(csv_name)
+
+    sheet_name = 'Books'
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    ws = writer.sheets[sheet_name]
+
+    ws.set_column('A:A', 30)
+    ws.set_column('B:C', 40)
+    ws.set_column('D:D', 75, text_wrap_format)
+    ws.set_column('E:E', 55, number_format)
+    ws.set_column('F:F', 20)
+    writer.save()
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-prefix')
+    return parser.parse_args()
+
+
+def get_info(prefix):
+    with open(f'inputs/{prefix}.url') as fr:
+        url = fr.read().strip()
+
+    covered_books = set()
+    with open(f'inputs/{prefix}.covered.txt') as fr:
+        for line in fr:
+            covered_books.add(line.strip())
+    return url, covered_books
+
+
+def main():
+    args = setup_args()
+    logging.info(args)
+
+    series_url, covered_books = get_info(args.prefix)
+    logging.info(covered_books)
+    logging.info(f'inputs/{args.prefix} URL: {series_url} #Covered Books: {len(covered_books)}')
+
+    csv_name = f'reports/report_{args.prefix}.csv'
+    report_name = f'reports/report_{args.prefix}.xlsx'
+    results_file = f'reports/{args.prefix}.books.pkl'
+
+    all_books = {}
+    if os.path.exists(results_file):
+        with open(results_file, 'rb') as fr:
+            all_books = pkl.load(fr)
+    else:
+        logging.info(f'Empty initial results')
+
+    logging.info(f'START #Books {len(all_books)}')
+
+    for page_num in range(1, 100):
+        author_url = f'{series_url}?page={page_num}&per_page=50'
+        logging.info(f'Page#{page_num}: {author_url}')
+        num = get_book_names_and_url(author_url, covered_books, all_books)
+        if not num:
+            logging.info(f'Stopping at Page: {page_num}')
+            break
+        write_csv(all_books, csv_name)
+        write_excel(csv_name, report_name)
+        logging.info(f'Saved Info for {len(all_books)}')
+
+        with open(results_file, 'wb') as fw:
+            pkl.dump(all_books, fw)
+        time.sleep(240)
+
+    logging.info(f'All Done!')
+    write_csv(all_books, csv_name)
+    write_excel(csv_name, report_name)
+    fw.close()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ find_series.py
@@ -0,0 +1,138 @@
+from bs4 import BeautifulSoup
+import argparse
+import csv
+import pandas as pd
+import requests
+import re
+import logging
+from utils import *
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+GR_URL = "http://goodreads.com"
+NEG_RESULT = "0 result(s)"
+
+
+def get_book_names_and_url(series_url, skip_books):
+    r = requests.get(series_url)
+    if r.status_code != 200:
+        logging.info(f'Series URL: bad request {r.status_code}')
+        return []
+
+    soup = BeautifulSoup(r.text, features="html.parser")
+
+    books = []
+    for book in soup.find_all(class_="gr-h3 gr-h3--serif gr-h3--noMargin"):
+        if book["href"].find("/book/show") == -1:
+            continue
+
+        book_name = book.text
+        if book_name in skip_books:
+            continue
+
+        logging.info(book_name)
+        book_url = f'{GR_URL}{book["href"]}'
+        logging.info(book_url)
+
+        book_editions_url = find_editions_page(book_url)
+        print(book_editions_url)
+
+        isbns = get_isbns(book_editions_url)
+        logging.info(f'#ISBNs {len(isbns)}')
+
+        bookchor_present = [
+            isbn
+            for isbn in isbns
+            if is_bookchor_instock(isbn)
+        ]
+
+        logging.info(f'#BookChor: {len(bookchor_present)}')
+
+        shbi_present = is_shbi_instock(book_name)
+        logging.info(f'shbi: {shbi_present}')
+
+        books.append((book_name, book_url, book_editions_url, isbns, bookchor_present, shbi_present))
+        logging.info(f'Done Book# {len(books)}')
+    return books
+
+
+def write_csv(books, csv_name):
+    with open(csv_name, 'w') as fw:
+        writer = csv.writer(fw)
+        writer.writerow(['Book Name', 'URL', 'Editions', 'ISBNS', 'Bookchor', 'SHBI'])
+        for book in books:
+            datum = []
+            datum.append(book[0])
+            datum.append(book[1])
+            datum.append(book[2])
+            isbns = [
+                str(num)
+                for num in book[3]
+            ]
+
+            bc_isbns = [
+                str(num)
+                for num in book[4]
+            ]
+
+            datum.append(','.join(isbns))
+            datum.append(','.join(bc_isbns))
+            datum.append(book[5])
+            writer.writerow(datum)
+
+
+def write_excel(csv_name, report_name):
+    writer = pd.ExcelWriter(report_name, engine='xlsxwriter')
+    workbook = writer.book
+    text_wrap_format = workbook.add_format({'text_wrap': True})
+
+    number_format = workbook.add_format({'num_format': '#'})
+
+    df = pd.read_csv(csv_name)
+
+    sheet_name = 'Books'
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    ws = writer.sheets[sheet_name]
+
+    ws.set_column('A:A', 30)
+    ws.set_column('B:C', 40)
+    ws.set_column('D:D', 75, text_wrap_format)
+    ws.set_column('E:E', 55, number_format)
+    ws.set_column('F:F', 20)
+    writer.save()
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-prefix')
+    return parser.parse_args()
+
+
+def get_info(prefix):
+    with open(f'inputs/{prefix}.url') as fr:
+        url = fr.read().strip()
+
+    covered_books = set()
+    with open(f'inputs/{prefix}.covered.txt') as fr:
+        for line in fr:
+            covered_books.add(line.strip())
+    return url, covered_books
+
+
+def main():
+    args = setup_args()
+    logging.info(args)
+
+    series_url, covered_books = get_info(args.prefix)
+    logging.info(f'inputs/{args.prefix} URL: {series_url} #Covered Books: {len(covered_books)}')
+
+    books = get_book_names_and_url(series_url, covered_books)
+    csv_name = f'reports/report_{args.prefix}.csv'
+    report_name = f'reports/report_{args.prefix}.xlsx'
+
+    write_csv(books, csv_name)
+    write_excel(csv_name, report_name)
+
+
+if __name__ == '__main__':
+    main()
+
